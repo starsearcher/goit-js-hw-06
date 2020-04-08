b92e7501bb4664e6ec0c141e1f880eef0a226708
starsearcher <57115514+starsearcher@users.noreply.github.com>
2020-04-08 22:42:45 +0300

    v 2.0

--- task-06.js
+++ task-06.js
@@ -1,7 +1,7 @@
 import users from "./users.js";
 
 const getUsersWithAge = (users, min, max) =>
-    users.filter(user => (user.age > min) & (user.age < max));;
+    users.filter(user => (user.age > min) && (user.age < max));;
 
 console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
 
